b889ccdf77f4c28682f1359250d29ba66902129d
tasess <sessions.tay@gmail.com>
2019-07-15 21:26:24 -0400

    Saving progress

--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,50 @@
 import React, { Component } from "react";
+import {
+    Route,
+    NavLink,
+    HashRouter
+} from "react-router-dom";
+import Menu from "./Menu";
 
 class Home extends Component {
     render() {
     return (
+        <HashRouter>
         <div>
             <div className="jumbotron">
-                <h1>Bon Appetit</h1>
+                <h1>Sweet Life</h1>
 
                 <button className="btn btn-primary"><i class="fas fa-cookie-bite"></i>  Order Now</button>
             </div>
             <div>
+                
                 <div className="row no-gutters">
                     <div className="col-12 col-sm-4">
-                        <div className="card card-block card-pastry">
-                            One
-                        </div>
+                        <NavLink to="/Menu" className="card-link">
+                            <div className="card card-block card-pastry">
+                                <h2>Pastries</h2>
+                            </div>
+                        </NavLink> 
                     </div>
                     <div className="col-12 col-sm-4">
-                        <div className="card card-block card-coffee">
-                        Two
-                        </div>
+                        <NavLink to="/Menu" className="card-link">
+                            <div className="card card-block card-coffee">
+                                <h2>Coffee</h2>
+                            </div>
+                        </NavLink>
                     </div>
                     <div className="col-12 col-sm-4">
-                        <div className="card card-block card-cake">
-                        Three
-                        </div>
+                        <NavLink to="/Menu" className="card-link">
+                            <div className="card card-block card-cake">
+                                <h2>Cake</h2>
+                            </div>
+                        </NavLink>
                     </div>
                 </div>
             </div>
+            <Route path="/Menu" component={Menu}/>
         </div>
+        </HashRouter>
     );
 }
 }
---
+++ src/Menu.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Menu extends Component {
+    render() {
+    return (
+        <div>
+            
+            <h2>Menu page!</h2>
+
+        </div>
+    );
+}
+}
+
+export default Menu;

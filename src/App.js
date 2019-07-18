c3da54ecd78f901f0721bf8cefdd1cdc3aee59e3
tasess <sessions.tay@gmail.com>
2019-07-18 13:51:22 -0400

    Minor additions

--- src/App.js
+++ src/App.js
@@ -1,65 +1,19 @@
 import React, { Component } from "react";
-import {
-    Route,
-    NavLink,
-    HashRouter
-} from "react-router-dom";
-import Home from "./Home";
-import About from "./About";
-import Contact from "./Contact";
-import Menu from "./Menu";
-import './App.css';
 
-class Header extends Component {
+import './App.css';
+import Header from "./Header";
+import Footer from "./Footer";
+
+class App extends Component {
     render() {
     return (
-        <HashRouter>
         <div>
-        <nav className="navbar navbar-expand-lg navbar-dark">
-            <NavLink to="/" className="navbar-brand" >
-            <i className="fas fa-cheese"></i>  Bon Appetite
-            </NavLink>
+            <Header />
 
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                        <NavLink to="/" exact={true} className="nav-link" >
-                        <i class="fa fa-home"></i>  Home
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/About" className="nav-link" >
-                        <i class="fa fa-book"></i> About
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/Contact" className="nav-link" >
-                        <i className="fa fa-envelope"></i> Contact
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/Menu" className="nav-link" >
-                        <i className="fas fa-utensils"></i> Menu
-                        </NavLink>
-                    </li>
-                </ul>
-            </div>
-
-
-        </nav>
-        <div className="content">
-                <Route exact path="/" component={Home}/>
-                <Route path="/About" component={About}/>
-                <Route path="/Contact" component={Contact}/>
-                <Route path="/Menu" component={Menu}/>
-            </div>
+            <Footer />
         </div>
-        </HashRouter>
     );
 }
 }
 
-export default Header;
+export default App;
--- src/Home.js
+++ src/Home.js
@@ -9,42 +9,52 @@ import Menu from "./Menu";
 class Home extends Component {
     render() {
     return (
-        <HashRouter>
         <div>
-            <div className="jumbotron">
-                <h1>Sweet Life</h1>
-
-                <button className="btn btn-primary"><i class="fas fa-cookie-bite"></i>  Order Now</button>
-            </div>
+            <HashRouter>
             <div>
-                
-                <div className="row no-gutters">
-                    <div className="col-12 col-sm-4">
-                        <NavLink to="/Menu" className="card-link">
-                            <div className="card card-block card-pastry">
-                                <h2>Pastries</h2>
-                            </div>
-                        </NavLink> 
-                    </div>
-                    <div className="col-12 col-sm-4">
-                        <NavLink to="/Menu" className="card-link">
-                            <div className="card card-block card-coffee">
-                                <h2>Coffee</h2>
-                            </div>
-                        </NavLink>
-                    </div>
-                    <div className="col-12 col-sm-4">
-                        <NavLink to="/Menu" className="card-link">
-                            <div className="card card-block card-cake">
-                                <h2>Cake</h2>
-                            </div>
-                        </NavLink>
+                <div className="jumbotron">
+                    <h1>Sweet Life</h1>
+
+                    <button className="btn btn-primary"><i class="fas fa-cookie-bite"></i>  Order Now</button>
+                </div>
+                <div>
+                    
+                    <div className="row no-gutters">
+                        <div className="col-12 col-sm-4">
+                            <NavLink to="/Menu" className="card-link">
+                                <div className="card card-block card-pastry">
+                                    <h2>Pastries</h2>
+                                </div>
+                            </NavLink> 
+                        </div>
+                        <div className="col-12 col-sm-4">
+                            <NavLink to="/Menu" className="card-link">
+                                <div className="card card-block card-coffee">
+                                    <h2>Coffee</h2>
+                                </div>
+                            </NavLink>
+                        </div>
+                        <div className="col-12 col-sm-4">
+                            <NavLink to="/Menu" className="card-link">
+                                <div className="card card-block card-cake">
+                                    <h2>Cake</h2>
+                                </div>
+                            </NavLink>
+                        </div>
                     </div>
                 </div>
+                <Route path="/Menu" component={Menu}/>
             </div>
-            <Route path="/Menu" component={Menu}/>
+            </HashRouter>
+
+            <div className="col-12 col-sm-8 container-fluid">
+                <blockquote className="blockquote text-center">
+                <p className="mb-0 display-4">Take time and enjoy a treat</p>
+                <footer className="blockquote-footer">We just think you deserve it!</footer>
+                </blockquote>
+            </div>
+
         </div>
-        </HashRouter>
     );
 }
 }

908a96ac5b85a1944b5faa9a94da1d491e4656dd
tasess <sessions.tay@gmail.com>
2019-08-12 18:40:09 -0400

    Completed Footer

--- src/Footer.js
+++ src/Footer.js
@@ -6,23 +6,26 @@ class Footer extends Component {
         <div>
             <footer>
                 <div className="row footer text-center">
-                        <div className="col-12 col-md-4">
+                        <div className="col-12 col-md-4 location">
                             <h3 className="display-5">Come visit!</h3>
                             <p>
                                 3209 Caramel Grove <br />
                                 Ice Cream, Cone, 54216
                             </p>
                         </div>
-                        <div className="col-12 col-md-3">
-                            <p>Some stuff</p>
+                        <div className="col-12 col-md-4 social-media text-center">
+                            <h3 className="display-5">Follow us on social media:</h3>
+                            <a href="http://www.facebook.com" target="__blank"><i class="fab fa-facebook-square"></i></a>
+                            <a href="http://www.twitter.com" target="__blank"><i class="fab fa-twitter-square"></i></a>
+                            <a href="http://www.instagram.com" target="__blank"><i class="fab fa-instagram"></i></a>
                         </div> 
-                        <div className="col-12 col-md-3 logo">
+                        <div className="col-12 col-md-4 footer-logo">
                             
                         </div>
 
                 </div>
                 <div className="footer-copyright text-center">
-                    <p>© Copyright 2019 Bon Appetite. All rights reserved</p>
+                    <p>© Copyright 2019 <b>Cafe Business Inc.</b>. All rights reserved</p>
                 </div>
             </footer>
         </div>

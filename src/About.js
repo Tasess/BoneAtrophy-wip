e93fecd3be17fff833584b344ceb923ea1634ca4
tasess <sessions.tay@gmail.com>
2019-08-12 18:40:35 -0400

    Beginning to work on About, parallax added

--- src/About.js
+++ src/About.js
@@ -4,7 +4,16 @@ class About extends Component {
     render() {
     return (
         <div>
-            <h2>About Us</h2>
+            <div className="row">
+                <div className="col-12 about-header">
+                    <h1 className=" display-4">
+                        We're Just Coffee Lovers ♥
+                    </h1>
+                    <p>Small and Locally owned. We just want to spread the joy of relaxing and enjoying fresh baked cake and a warm cup of coffee. </p>
+                </div>
+                
+            </div>
+            <div className="jumbotron about-one jumbotron-fluid"></div>
         </div>
     );
 }

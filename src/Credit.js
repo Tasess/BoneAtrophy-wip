6ec445e0c275920d8447d4e56aa66ff5cd85b5d3
tasess <sessions.tay@gmail.com>
2019-08-15 23:54:09 -0400

    fixed security issue with link

--- src/Credit.js
+++ src/Credit.js
@@ -15,7 +15,7 @@ class Credit extends Component {
                     
                     <div>
                         Cafe Lineral:
-                        Icon made by <a href="https://www.flaticon.com/authors/wanicon" title="wanicon">wanicon</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"             title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+                        Icon made by <a href="https://www.flaticon.com/authors/wanicon" title="wanicon">wanicon</a> from <a href="https://www.flaticon.com/"             title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/"             title="Creative Commons BY 3.0" target="_blank" rel="noopener noreferrer">CC 3.0 BY</a></div>
                 </div>
                 <div>
                 Daisy Floral Cake:
